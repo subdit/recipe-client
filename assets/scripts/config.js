455c58ec4932e5beac7ef54a73456129370dce5e
kasiditbualoeng <subdit@gmail.com>
2018-09-05 16:11:45 -0400

    remove < ...>

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<https://enigmatic-tor-32625.herokuapp.com>',
+  production: 'https://enigmatic-tor-32625.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
